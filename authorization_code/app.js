6a922d2747b19d664a912c1f6f974436d39d5cdf
wdfarr <wdfarr123@gmail.com>
2022-11-28 08:09:00 -0500

    Adding my credentials

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = 'cabab0a054684bd68f54bf006c664b76'; // Your client id
+var client_secret = '8e2eb871e7fb4a7d9805a1209772678a'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
